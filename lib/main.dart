fd443928657b2ad7602aefdd8da0ab938accf9e3
Amanda Silva <amandarodsilva4@gmail.com>
2024-02-05 09:04:42 -0300

    AAAAA

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   final List<Product> products = [
     Product(
         image: 'dunk_blue.png',
-        name: 'Tenis Nike Dunk Low Retro Se Masculino',
+        name: 'Tenis Nike Dunk Low Retro Se Masculinos',
         description:
             'Avançar rápido. Rebobinar. Não importa – este tênis leva o retrô para o futuro. O V2K remasteriza tudo o que você adora no Vomero em um visual retirado diretamente de um catálogo de corrida dos primeiros anos. Camadas com uma mistura de azuis gelados, detalhes translúcidos e uma entressola macia com uma estética perfeitamente vintage. E uma sola grossa garante que você se sinta confortável onde quer que vá.',
         price: 899.99),
